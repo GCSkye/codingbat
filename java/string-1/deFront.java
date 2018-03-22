4563a0a68e7ea0bcc0a74d2ca1ba743840046542
GCSkye <10370472+GCSkye@users.noreply.github.com>
2018-03-22 13:35:31 -0400

    Update deFront.java
    Simplified.

--- java/string-1/deFront.java
+++ java/string-1/deFront.java
@@ -3,18 +3,8 @@
  * string may be any length. Harder than it looks.
  */
 public String deFront(String str) {
-    if(str.length() == 1 && str.charAt(0) != 'a')
-        return "";
-          
-    if(str.length() >= 2) {
-        if(str.charAt(0) != 'a' && str.charAt(1) != 'b') {
-            return str.substring(2);
-        } else if(str.charAt(0) != 'a') {
-            return str.substring(1);
-        } else if(str.charAt(1) != 'b') {
-            return "a" + str.substring(2);
-        }
-    }
-                                                  
-    return str;
+  String holder = "";
+  if (str.charAt(0) == 'a') holder+="a";
+  if (str.charAt(1) == 'b') holder+="b";
+  return holder += str.substring(2);
 }
